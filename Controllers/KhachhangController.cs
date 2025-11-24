09d98051f6ca218bce8cdb5a36f98a141d93d753
vydang <vydang332@gmail.com>
2025-11-24 22:28:47 +0700

    Add files via upload

---
+++ Controllers/KhachhangController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ABCShop.Data;
+using ABCShop.Models;
+
+namespace ABCShop.Controllers
+{
+    public class KhachhangController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public KhachhangController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Khachhang
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Khachhang.ToListAsync());
+        }
+
+        // GET: Khachhang/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachhang = await _context.Khachhang
+                .FirstOrDefaultAsync(m => m.MaKh == id);
+            if (khachhang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachhang);
+        }
+
+        // GET: Khachhang/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Khachhang/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MaKh,Ten,DienThoai,Email,MatKhau")] Khachhang khachhang)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(khachhang);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khachhang);
+        }
+
+        // GET: Khachhang/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachhang = await _context.Khachhang.FindAsync(id);
+            if (khachhang == null)
+            {
+                return NotFound();
+            }
+            return View(khachhang);
+        }
+
+        // POST: Khachhang/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MaKh,Ten,DienThoai,Email,MatKhau")] Khachhang khachhang)
+        {
+            if (id != khachhang.MaKh)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(khachhang);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!KhachhangExists(khachhang.MaKh))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(khachhang);
+        }
+
+        // GET: Khachhang/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var khachhang = await _context.Khachhang
+                .FirstOrDefaultAsync(m => m.MaKh == id);
+            if (khachhang == null)
+            {
+                return NotFound();
+            }
+
+            return View(khachhang);
+        }
+
+        // POST: Khachhang/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var khachhang = await _context.Khachhang.FindAsync(id);
+            if (khachhang != null)
+            {
+                _context.Khachhang.Remove(khachhang);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool KhachhangExists(int id)
+        {
+            return _context.Khachhang.Any(e => e.MaKh == id);
+        }
+    }
+}
